ed7a3a327a4cad94eddc2b470d5bed1cff522b51
Nghia Nguyen Duc Dai <nghia.nguyenduc@axonactive.com>
2020-12-18 17:30:18 +0700

    Init Config

---
+++ config/src/test/java/dnj/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package dnj.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
