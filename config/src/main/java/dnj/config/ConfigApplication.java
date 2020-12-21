3890d854311e50dd8feecf55b5b89e2589da3732
Nghia Nguyen Duc Dai <nghia.nguyenduc@axonactive.com>
2020-12-21 10:09:36 +0700

    Init user-service

--- config/src/main/java/dnj/config/ConfigApplication.java
+++ config/src/main/java/dnj/config/ConfigApplication.java
@@ -2,7 +2,7 @@ package dnj.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
