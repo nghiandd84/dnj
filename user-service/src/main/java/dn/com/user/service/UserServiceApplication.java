b1ccab92c16d6e96056066abdee3cec300a77095
Nghia Nguyen Duc Dai <nghia.nguyenduc@axonactive.com>
2020-12-22 10:52:46 +0700

    Use config server on user-service

--- user-service/src/main/java/dn/com/user/service/UserServiceApplication.java
+++ user-service/src/main/java/dn/com/user/service/UserServiceApplication.java
@@ -1,11 +1,13 @@
 package dn.com.user.service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAutoConfiguration
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
