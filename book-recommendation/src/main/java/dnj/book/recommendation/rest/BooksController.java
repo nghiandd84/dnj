58c9ceb326ec2d16bfe225213b2784049ad81e6a
Nghia Nguyen Duc Dai <nghia.nguyenduc@axonactive.com>
2020-12-23 10:25:08 +0700

    add sleuth to tracing

--- book-recommendation/src/main/java/dnj/book/recommendation/rest/BooksController.java
+++ book-recommendation/src/main/java/dnj/book/recommendation/rest/BooksController.java
@@ -1,29 +1,26 @@
 package dnj.book.recommendation.rest;
 
-import org.slf4j.LoggerFactory;
-
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.common.base.Optional;
-
 import dnj.book.recommendation.model.BookRecommendation;
 import dnj.book.recommendation.service.BookCatalogService;
 import dnj.book.recommendation.service.BookInventoryService;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping(value = "/books", produces = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
 public class BooksController {
 
-	private Logger log = LoggerFactory.getLogger(BooksController.class);
+//	private Logger log = LoggerFactory.getLogger(BooksController.class);
 
 	@Autowired
 	private BookCatalogService bookCatalogService;
@@ -33,11 +30,7 @@ public class BooksController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public List<BookRecommendation> getRecommendation() {
-		this.bookCatalogService.findAll().stream().forEach(book -> {
-			log.info("Find Book {}", book);
-			Boolean isInStock = this.bookInventoryService.haveInInventory(book.getId());
-			log.info("isInStock {}", isInStock);
-		});
+		log.info("getRecommendation");
 		return this.bookCatalogService.findAll().stream().filter(book -> {
 			Boolean haveInInventory = this.bookInventoryService.haveInInventory(book.getId());
 			return Objects.nonNull(haveInInventory) && haveInInventory;
